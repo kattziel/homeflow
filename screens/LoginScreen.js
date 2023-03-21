64258411125c497a2d8d6ad40d036a98f00bbf7f
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-21 16:34:27 +0100

    error handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,26 @@
-import { View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { useState } from "react";
+
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import AuthContent from "../components/Auth/AuthContent";
+import { authenticate } from '../util/auth';
 
 function LoginScreen() {
-  function loginHandler() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+     const response = await authenticate({ email, password });
+     console.log(response.data);
+    } catch (error) {
+      console.log(error, "error");
+    }
+
+    setIsAuthenticating(false);
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging user in..." />;
   }
   return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyCBOOU19tedM_f6xOykfJDfgbQVUU0ZLYA";
+const API_KEY = 'AIzaSyCBOOU19tedM_f6xOykfJDfgbQVUU0ZLYA';
+
+export async function authenticate(email, password) {
+  const response = await axios.post(
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + API_KEY, {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }
+  )
+}
 
 export async function createUser(email, password) {
   const response = await axios.post(

63698631d8b54b979e13ae662dd182263edebf6b
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-21 15:55:31 +0100

    adding loading overlay

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,13 +1,23 @@
 import AuthContent from "../components/Auth/AuthContent";
+import { useState } from "react";
+
 import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  function signupHandler() {
-    createUser();
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
   }
   // connecting signupHandler to the AuthForm, cause it is where we are gathering the data
-  return <AuthContent onAuthenticate={signupHandler}/>;
+  return <AuthContent onAuthenticate={signupHandler} />;
+  // AuthContent gets an onAuthenticated prop, e.g. the function that is triggered when all the inputs were filled correctly;
 }
 
 export default SignupScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,14 @@
 import axios from "axios";
+
 const API_KEY = "AIzaSyCBOOU19tedM_f6xOykfJDfgbQVUU0ZLYA";
 
-export async function createUser({ email, password }) {
+export async function createUser(email, password) {
   const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
       email: email,
       password: password,
-      returnSecureToken: true,
+      returnSecureToken: true
     }
   );
 }

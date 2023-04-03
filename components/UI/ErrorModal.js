c6ce41d8416f5534c9d178fd233a5a7e1f8f2263
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-04-03 16:55:19 +0200

    added quit button in error modal screen

--- components/UI/ErrorModal.js
+++ components/UI/ErrorModal.js
@@ -1,31 +1,47 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { useState } from "react";
 
 import { Ionicons } from "@expo/vector-icons";
 
 function ErrorModal() {
+  const [errorModalIsVisible, setErrorModalIsVisible] = useState(true);
+
+  function quitErrorModal() {
+    setErrorModalIsVisible(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.contentContainer}>
-        <View style={styles.exitButtonContainer}>
-          <Ionicons name={"close-outline"} size={25} color="gray"></Ionicons>
-        </View>
-        <View style={styles.errorContainer}>
-          <Text style={styles.errorText}>Error</Text>
-        </View>
-        <View style={styles.headerContainer}>
-          <Text style={styles.header}>Ooops, that appears incorrect ..</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>
-            Are you sure you typed it correctly or could it be another password?
-            Why not give it another go? If you can't remember it, ask your
-            family for shared family password or - if you've set a personal
-            password - you can reset it by tapping "I forgot my password" button
-            below.
-          </Text>
+    errorModalIsVisible && (
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <View style={styles.exitButtonContainer}>
+            <Pressable>
+              <Ionicons
+                name={"close-outline"}
+                size={25}
+                color="gray"
+                onPress={quitErrorModal}
+              ></Ionicons>
+            </Pressable>
+          </View>
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>Error</Text>
+          </View>
+          <View style={styles.headerContainer}>
+            <Text style={styles.header}>Ooops, that appears incorrect ..</Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>
+              Are you sure you typed it correctly or could it be another
+              password? Why not give it another go? If you can't remember it,
+              ask your family for shared family password or - if you've set a
+              personal password - you can reset it by tapping "I forgot my
+              password" button below.
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
+    )
   );
 }
 
